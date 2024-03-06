a387b875b43a459c6df5bc716b336be2407cb0dc
Tomer Zarhin <tomerza2@ac.sce.ac.il>
2024-03-06 10:14:30 +0200

    ejs file for landing page

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
+import ejs from "ejs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 3000;
 
-//connect to mongo db
+// Connect to MongoDB
 const dbURI = "mongodb+srv://tomerza2:ciLDOAeMlZboe3FB@blogtest.bq06yi6.mongodb.net/blogs?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI)
@@ -17,8 +24,19 @@ mongoose.connect(dbURI)
   })
   .catch((err) => console.log(err));
 
-app.use(express.static("public"));
+  // Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Specify the directory where your views are located
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from the frontend/public directory
+app.use(express.static('../frontend/public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Serve the landing page
+app.get("/",(req,res)=>{
+  res.render("landingpage");
+});
 
