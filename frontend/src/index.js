73f0f47d88d8df141c50120a455aa8036e241c02
Tomer Zarhin <tomerza2@ac.sce.ac.il>
2024-03-07 13:15:07 +0200

    updating the backend  index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LandingPage from './pages/landing/LandingPage.js';
+import LandingPage from './pages/landing/LandingPage';
+import TeacherMainPage from './pages/teacher/TeacherMainPage';
 
+// For landing page
 ReactDOM.render(
   <React.StrictMode>
-    <LandingPage /> {/* Render the LandingPage component */}
+    <LandingPage />
   </React.StrictMode>,
   document.getElementById('root')
+);
+
+// For teacher page
+ReactDOM.render(
+  <React.StrictMode>
+    <TeacherMainPage />
+  </React.StrictMode>,
+  document.getElementById('teacher-root')
 );
