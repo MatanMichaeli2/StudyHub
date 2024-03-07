08951d0e30dfba5398edb998dd6c8ff400b89465
hanahozias <hoziashana@gmail.com>
2024-03-07 21:49:55 +0200

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Axios from "axios";
+import AdminDisplay from './components/UsersDetails/AdminDisplay';
+
 
 const App = () => {
 
@@ -16,9 +18,17 @@ const App = () => {
   },[]);
 
   return(
-    <div>{data}</div>
+    <div>
+
+    
+      {data}
+    
+    
+    </div>
   )
   
 }
 
-export default App
+export default App;
+
+
---
+++ frontend/src/components/UsersDetails/Actions.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+//import './Actions.css';
+
+const Actions = () => {
+  return (
+    <div>
+      <button>Edit User</button>
+      <button>Delete User</button>
+    </div>
+  );
+};
+
+export default Actions;
