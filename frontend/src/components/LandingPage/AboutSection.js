cf8ed725e63b7214b299fafe2dc2988e04212fcb
Matanmichaeli2 <matanmi2@ac.sce.ac.il>
2024-03-21 16:16:46 +0200

    Landing page pictures fix

--- frontend/src/components/LandingPage/AboutSection.js
+++ frontend/src/components/LandingPage/AboutSection.js
@@ -8,7 +8,7 @@ function AboutSection() {
         <p>Welcome to StudyHub, your premier destination for study groups online. Our mission is to provide a collaborative and productive environment for students to excel academically.</p>
         <p>At StudyHub, we believe in the power of teamwork and knowledge sharing. Join us and elevate your learning experience with like-minded individuals who are committed to achieving academic success.</p>
       </div>
-      <img src="../../public/images/conflict750x500AdobeStock_341001801-750x500.jpg" alt="Study group" style={{ height: '50px', width: '50px' }} />
+      <img src="../../images/about-us-pic.webp" alt="Study group" style={{ height: '400px', width: '600px' }} />
     </div>
   );
 }
